316b1278d66694622e06ccf29fd5d48415723ace
devaneyj3 <jordandevaney28@gmail.com>
2025-09-04 13:51:27 -0400

    add js object and sort by date

--- app/(dashboard)/profile/page.jsx
+++ app/(dashboard)/profile/page.jsx
@@ -1,44 +1,12 @@
 import React from "react";
 import Stats from "@/components/Stats/Stats";
 import AllJobs from "@/components/AllJobs/AllJobs";
+import { jobs } from "@/APPLICATIONS";
 
 export const metadata = {
 	title: "Profile | Job Tracker",
 	description: "Track your jobs",
 };
-
-//   TODO: What and how should I display this data? What properties should I use? jobTitle, companyName, jobUrl, appliedDate, statusDate, archived, salary, location, contactName, contactEmail, nextFollowUpDate, lastContactedDate, jobDescription, interviewNotes, interviewDate, offerDetails, rejectionReason, dateArchived, notes
-
-const jobs = [
-	{
-		id: 1,
-		title: "Full Stack Engineer",
-		company: "Gravity",
-		appliedDate: "9/2/25",
-		status: "Applied",
-	},
-	{
-		id: 2,
-		title: "Backend Engineer",
-		company: "Beta",
-		appliedDate: "9/2/25",
-		status: "Interview",
-	},
-	{
-		id: 3,
-		title: "Backend Engineer",
-		company: "Beta",
-		appliedDate: "9/2/25",
-		status: "Applied",
-	},
-	{
-		id: 4,
-		title: "Backend Engineer",
-		company: "Beta",
-		appliedDate: "9/2/25",
-		status: "Offer",
-	},
-];
 
 export default function ProfilePage() {
 	return (
--- components/AllJobs/AllJobs.jsx
+++ components/AllJobs/AllJobs.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import styles from "./AllJobs.module.scss";
 
 export default function AllJobs({ jobs }) {
+	const sortedJobs = jobs.sort(
+		(a, b) => new Date(b.appliedDate) - new Date(a.appliedDate)
+	);
+
 	return (
 		<main className={styles.container}>
 			<h1 className={styles.title}>All Jobs</h1>
 
 			<ul className={styles.list}>
-				{jobs.map((j) => (
-					<li key={j.id} className={styles.item}>
-						<div className={styles.jobTitle}>{j.title}</div>
-						<div className={styles.company}>{j.company}</div>
-						<div className={styles.company}>{j.status}</div>
-						<div className={styles.company}>{j.appliedDate}</div>
+				{sortedJobs.map((j, index) => (
+					<li key={index} className={styles.item}>
+						<div className={styles.jobTitle}>{j.jobTitle}</div>
+						<div className={styles.company}>{j.companyName}</div>
+						<div className={styles.status}>{j.status}</div>
+						<div className={styles.appliedDate}>{j.appliedDate}</div>
 					</li>
 				))}
 			</ul>
